bb0e8cf4b6b8e68e661cd288bd1a2fb288faaa32
Kelsey Crompton <kelsey.crompton@gmail.com>
2023-02-15 21:43:30 -0500

    Added CategoryService and controller

---
+++ BlazorEcommerce/Server/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService CategoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            CategoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategories()
+        {
+            var result = await CategoryService.GetCategoriesAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DataContext Context;
+
+        public CategoryService(DataContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+        {
+            var categories = await Context.Categories.ToListAsync();
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
+    }
+}
